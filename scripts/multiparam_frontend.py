40773c5520adfd4d1172bf525f8495ab717048bb
Yu-Tang Peng <pengy25@cs.washington.edu>
2017-12-20 20:28:27 -0800

    Reorganized the layout of list frame

--- scripts/multiparam_frontend.py
+++ scripts/multiparam_frontend.py
@@ -3,34 +3,53 @@ import Tkinter as tk
 import rospy
 from single_param_frontend import ParamFrame
 
-class ItemFrame(tk.Frame):
-  def __init__(self, container):
-    tk.Frame.__init__(self, container)
-    self.deleteButton = tk.Button(self, text="-", command=self.callback)
-    self.paramFrame = ParamFrame(self)
-    self.deleteButton.pack(side=tk.LEFT)
-    self.paramFrame.pack(side=tk.LEFT, fill=tk.X, expand=tk.YES)
-
-  def callback(self):
-    self.deleteButton.pack_forget()
-    self.paramFrame.pack_forget()
-
-    self.deleteButton.destroy()
-    self.paramFrame.destroy()
-
-    tk.Frame.pack_forget(self)
-    tk.Frame.destroy(self)
-
 class ListFrame(tk.Frame):
   def __init__(self, container):
     tk.Frame.__init__(self, container)
-    self.addButton = tk.Button(self, text="+", width=80, command=self.callback)
-    self.addButton.pack(fill=tk.X, expand=tk.YES)
+    tk.Frame.grid_columnconfigure(self, 1, weight=1)
+    self.addButton = tk.Button(self, text="+", command=self.add_callback)
+    self.addButton.grid(row=0, columnspan=2, sticky=tk.W + tk.E)
+    self.itemLst = []
+    self.counter = 0
 
-  def callback(self):
-    self.addButton.pack_forget()
-    ItemFrame(self).pack(fill=tk.X, expand=tk.YES)
-    self.addButton.pack(fill=tk.X, expand=tk.YES)
+  def emptyFrame(self):
+    self.addButton.grid_forget()
+
+    for serial_num, deleteButton, item in self.itemLst:
+      deleteButton.grid_forget()
+      item.grid_forget()
+
+  def fillFrame(self):
+    for i in range(len(self.itemLst)):
+      self.itemLst[i][1].grid(row=i, column=0)
+      self.itemLst[i][2].grid(row=i, column=1, sticky=tk.W+tk.E)
+    
+    self.addButton.grid(row=len(self.itemLst), columnspan=2,  sticky=tk.W + tk.E)
+
+  def add_callback(self):
+    self.emptyFrame()
+
+    item = ParamFrame(self)
+    deleteButton = tk.Button(self, text="-", command=lambda num=self.counter: self.delete_callback(num))
+    self.itemLst.append((self.counter, deleteButton, item))
+    self.counter += 1
+
+    self.fillFrame()
+
+  def delete_callback(self, num):
+    self.emptyFrame()
+
+    tmp = self.itemLst
+    self.itemLst = []
+
+    for serial_num, deleteButton, item in tmp:
+      if (serial_num != num):
+        self.itemLst.append((serial_num, deleteButton, item))
+      else:
+        deleteButton.destroy()
+        item.destroy()
+
+    self.fillFrame()
 
 def main():
   rospy.init_node("multiparam_node")
@@ -39,7 +58,7 @@ def main():
 
   window = tk.Tk()
   majorFrame = ListFrame(window)
-  majorFrame.pack(fill=tk.X, expand=tk.YES)
+  majorFrame.pack(fill=tk.BOTH, expand=tk.YES)
 
   window.mainloop()
 
