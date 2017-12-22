dc8e82518652fd11c672df07eeab501ed13ffdf2
Yu-Tang Peng <pengy25@cs.washington.edu>
2017-12-21 16:22:12 -0800

    Added a scrollbar to the param listing frontend

--- scripts/list_param_frontend.py
+++ scripts/list_param_frontend.py
@@ -5,15 +5,25 @@ import rospy
 class ParamList(tk.Frame):
   def __init__(self, container):
     tk.Frame.__init__(self, container)
-    self.paramList = tk.Listbox(self)
+    self.scrollbar = tk.Scrollbar(container)
+    self.listbox = tk.Listbox(self, yscrollcommand=self.scrollbar.set)
+    self.scrollbar.config(command=self.listbox.yview)
 
     params = rospy.get_param_names()
 
     for param in params:
-      self.paramList.insert(tk.END, param)
+      self.listbox.insert(tk.END, param)
 
-    self.paramList.pack(fill=tk.BOTH, expand=tk.YES)
-  
+    self.listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.YES)
+    self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+  def update(self):
+    self.listbox.delete(0, tk.END)
+
+    params = rospy.get_param_names()
+
+    for param in params:
+      self.listbox.insert(tk.END, param)
 
 def main():
   rospy.init_node("list_param_node")
@@ -22,8 +32,16 @@ def main():
 
   window = tk.Tk()
 
-  majorFrame = ParamList(window)
-  majorFrame.pack(fill=tk.BOTH, expand=tk.YES)
+  frame1 = tk.Frame(window)
+  frame1.pack(fill=tk.BOTH, expand=tk.YES)
+
+  paramList = ParamList(frame1)
+  paramList.pack(fill=tk.BOTH, expand=tk.YES)
+
+  frame2 = tk.Frame(window)
+  frame2.pack(fill=tk.X, expand=tk.YES)
+
+  tk.Button(frame2, text="Update", command=paramList.update).pack(side=tk.RIGHT)
 
   tk.mainloop()
 
