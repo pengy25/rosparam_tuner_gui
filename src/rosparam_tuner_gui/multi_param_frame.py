ccde11be080e17c2e2751b90b69d0fee4a4cabc5
Yu-Tang Peng <pengy25@cs.washington.edu>
2018-01-02 16:53:41 -0800

    Used yaml module to detect value types. Fixed a bug in dump_file_reading_demo

--- src/rosparam_tuner_gui/multi_param_frame.py
+++ src/rosparam_tuner_gui/multi_param_frame.py
@@ -30,20 +30,16 @@ class MultiParamFrame(tk.Frame):
   def getParams(self):
     res = {}
     for serial_num, deleteButton, item in self.itemLst:
-      param_name, val_type, param_val = item.getParam()
-      if val_type == "int":
-        param_val = int(param_val)
-      elif val_type == "float":
-        param_val = float(param_val)
+      param_name, param_val = item.getParam()
 
       res[param_name] = param_val
 
     return res
 
-  def addParamFrame(self, param_name="param_name", param_val="param_val", val_type="string"):
+  def addParamFrame(self, param_name="param_name", param_val="param_val"):
     self.emptyFrame()
 
-    item = SingleParamFrame(self, param_name=param_name, param_val=param_val, val_type=val_type)
+    item = SingleParamFrame(self, param_name=param_name, param_val=param_val)
     deleteButton = tk.Button(self, text="-", command=lambda num=self.counter: self.deleteParamFrame(num))
     self.itemLst.append((self.counter, deleteButton, item))
     self.counter += 1
@@ -52,13 +48,8 @@ class MultiParamFrame(tk.Frame):
 
   def addParamFrames(self, params):
     for param_name, param_val in params.iteritems():
-      val_type = "string"
-      if type(param_val) is int:
-        val_type = "int"
-      elif type(param_val) is float:
-        val_type = "float"
 
-      self.addParamFrame(param_name, param_val, val_type)
+      self.addParamFrame(param_name, param_val)
 
   def deleteParamFrame(self, num):
     self.emptyFrame()
--- src/rosparam_tuner_gui/single_param_frame.py
+++ src/rosparam_tuner_gui/single_param_frame.py
@@ -1,17 +1,20 @@
 #! /usr/bin/env python
 import Tkinter as tk
 import rospy
+import yaml
 
 # Generic class for buttons connected with parameter name and value
 class ParamButton(tk.Button):
-  def __init__(self, container, text, param_name, param_val):
+  def __init__(self, container, text, param_name, param_val, val_type):
     self._param_name = param_name
     self._param_val = param_val
+    self._val_type = val_type
     tk.Button.__init__(self, container, text=text, command=self.callback)
 
   def callback(self):
     print "param name:", self._param_name.get()
     print "param val:", self._param_val.get()
+    print "val type:", self._val_type.get()
 
 # Button that supports 'rosparam get'
 class GetButton(ParamButton):
@@ -20,24 +23,37 @@ class GetButton(ParamButton):
 
   def callback(self):
     if rospy.has_param(self._param_name.get()):
-      self._param_val.set(rospy.get_param(self._param_name.get()))
+      target = rospy.get_param(self._param_name.get())
+      self._val_type.set(type(target))
+
+      if type(target) is bool:
+        if target:
+          target = "true"
+        else:
+          target = "false"
+
+      self._param_val.set(target)
+
     else:
-      self._param_val.set("param name not found") 
+      self._param_val.set("param name not found")
+      self._val_type.set("some undetermined value type") 
 
 # Button that supports 'rosparam set'
 class SetButton(ParamButton):
-  def __init__(self, container, text, param_name, val_type, param_val):
-    self._val_type = val_type
-    ParamButton.__init__(self, container, text, param_name, param_val)
+  def __init__(self, container, *args):
+    ParamButton.__init__(self, container, *args)
 
   def callback(self):
-    val = self._param_val.get()
-    if self._val_type.get() == "int":
-      val = int(val)
-    elif self._val_type.get() == "float":
-      val = float(val)
-    
-    rospy.set_param(self._param_name.get(), val)
+    yaml_str = self._param_name.get() + ": " + self._param_val.get()
+    res = yaml.safe_load(yaml_str)
+
+    if len(res) != 1:
+      print "Warning: a SetButton doesn't support multiple param setup"
+      return
+
+    for param_name, param_val in res.iteritems():
+      self._val_type.set(type(param_val))
+      rospy.set_param(param_name, param_val)
 
 # Button that supports 'rosparam delete'
 class DeleteButton(ParamButton):
@@ -52,7 +68,7 @@ class DeleteButton(ParamButton):
 
 # A single frame that provides interaction with a specified ROS parameter
 class SingleParamFrame(tk.Frame):
-  def __init__(self, container, param_name="param_name", param_val="param_value", val_type="string", *args):
+  def __init__(self, container, param_name="param_name", param_val="param_value", *args):
     tk.Frame.__init__(self, container, args)
     tk.Label(self, text="rosparam").pack(side=tk.LEFT)
 
@@ -61,24 +77,28 @@ class SingleParamFrame(tk.Frame):
     self.val_type = tk.StringVar()
     self.param_name.set(param_name)
     self.param_val.set(param_val)
-    self.val_type.set(val_type)  # Assume default type is string
+    self.val_type.set("some undetermined value type")
 
-    DeleteButton(self, "Delete", self.param_name, self.param_val).pack(side=tk.LEFT)
-    GetButton(self, "Get", self.param_name, self.param_val).pack(side=tk.LEFT)
-    SetButton(self, "Set", self.param_name, self.val_type, self.param_val).pack(side=tk.LEFT)
+    DeleteButton(self, "Delete", self.param_name, self.param_val, self.val_type).pack(side=tk.LEFT)
+    GetButton(self, "Get", self.param_name, self.param_val, self.val_type).pack(side=tk.LEFT)
+    self.setButton = SetButton(self, "Set", self.param_name, self.param_val, self.val_type)
+    self.setButton.pack(side=tk.LEFT)
 
     tk.Entry(self, textvariable=self.param_name).pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.YES)
-    tk.OptionMenu(self, self.val_type, "string", "int", "float").pack(side=tk.LEFT)
     tk.Entry(self, textvariable=self.param_val).pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.YES)
+    tk.Label(self, text=" of ").pack(side=tk.LEFT)
+    tk.Label(self, textvariable=self.val_type).pack(side=tk.LEFT)
 
   def getParam(self):
-    return (self.param_name.get(), self.val_type.get(), self.param_val.get())
+    yaml_str = self.param_name.get() + ": " + self.param_val.get()
+    res = yaml.safe_load(yaml_str)
+
+    if (len(res) != 1):
+      print "Warning: SingleParam detects multiple param_name"
+      return None
+
+    for param_name, param_val in res.iteritems():
+      return (param_name, param_val)
 
   def setParam(self):
-    val = self.param_val.get()
-    if self.val_type == "int":
-      val = int(val)
-    elif self.val_type == "float":
-      val = float(val)
-
-    rospy.set_param(self.param_name.get(), val)
+    self.setButton.callback()
